eda85f74d55e47556fd19f06486d45c72d47a55f
aatitkarki <aatitkarki123@gmail.com>
2023-04-30 10:21:36 +0545

    updated port to 4000

--- backend/src/app.js
+++ backend/src/app.js
@@ -50,6 +50,6 @@ app.get("/", (req, res) => {
   res.send("Welcome to the registration API!");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+app.listen(4000, () => {
+  console.log("Server running on http://localhost:4000");
 });
